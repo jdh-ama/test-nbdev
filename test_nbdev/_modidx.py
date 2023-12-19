493eeee3f21f0d4d0e996fb7efd3e7dac87a22bc
James D. Hasselman <noreplies@hasselman.org>
2023-12-19 16:13:26 +0000

    Export

--- test_nbdev/_modidx.py
+++ test_nbdev/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://james.d.hasselman@gmail.com.github.io',
                 'git_url': 'https://github.com/james.d.hasselman@gmail.com/test-nbdev',
                 'lib_path': 'test_nbdev'},
-  'syms': {'test_nbdev.core': {'test_nbdev.core.foo': ('core.html#foo', 'test_nbdev/core.py')}}}
+  'syms': { 'test_nbdev.core': {'test_nbdev.core.foo': ('core.html#foo', 'test_nbdev/core.py')},
+            'test_nbdev.sqrty': {'test_nbdev.sqrty.sqrty': ('sqrty.html#sqrty', 'test_nbdev/sqrty.py')}}}
